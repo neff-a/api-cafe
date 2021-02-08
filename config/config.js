204df8702571f4cb0e2be6583bb346f970177d3a
Neftali Alarcon <nalarcon@focaltec.com>
2021-02-08 17:45:08 -0600

    env mongo

--- config/config.js
+++ config/config.js
@@ -14,8 +14,4 @@ process.env.NODE_ENV = process.env.NODE_ENV ||  'dev';
 *   database 
 */
 
-if (process.env.NODE_ENV === 'dev') {
-    process.env.DATABASE = 'mongodb://localhost/cafe';
-} else {
-    process.env.DATABASE = 'mongodb+srv://master:PEPkVO9YAGRlDKZI@cluster-sandbox.gq64z.mongodb.net/cafe?retryWrites=true&w=majority';
-}
+process.env.MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost/cafe';
