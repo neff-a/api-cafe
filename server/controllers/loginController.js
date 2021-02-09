1aea04591826e5fdb700d6dec3da5e6030674e26
Neftali Alarcon <nalarcon@focaltec.com>
2021-02-09 11:38:07 -0600

    DEV: login user by email and password

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -6,6 +6,40 @@ const User = require('../models/user');
 
 app.post('/login', function(request, response) {
 
+    let userRequest = request.body;
+
+    User.findOne({ email: userRequest.email }, (error, userDB) => {
+
+        if(error) {
+            return response.status(500).json({
+                code: 500,
+                message: 'Internal server error',
+                error
+            })
+        }
+
+        if (!userDB) {
+            return response.status(400).json({
+                code: 400,
+                message: '(User) or password are not valid'
+            })
+        }
+
+        let matchPassword = bcrypt.compareSync(userRequest.password, userDB.password)
+
+        if (!matchPassword) {
+            return response.status(400).json({
+                code: 400,
+                message: 'User or (password) are not valid'
+            })
+        }
+
+        return response.json({
+            user: userDB,
+            token: 'xxx-xxx-xxx'
+        })
+    });
+
 });
 
 module.exports = app    
